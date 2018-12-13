6fafabd36b100cd799fb387f6e29bfa6d3d70cb3
ITDATA.Osorio Tello <gosoriot@proveedor.intercorp.com.pe>
2018-12-12 19:59:55 -0500

    ok

--- src/main/java/app/salesorder/application/dao/SalesorderDAO.java
+++ src/main/java/app/salesorder/application/dao/SalesorderDAO.java
@@ -13,6 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 import app.salesorder.domain.entity.Salesorder;
 import app.salesorderdetall.domain.entity.Saleorderdetall;
@@ -26,29 +30,36 @@ public class SalesorderDAO implements ISalesorderDAO {
 	public void setDataSource(DataSource dataSource) {
 		template = new JdbcTemplate(dataSource);
 	}
+	
+	
 
 	@Override
-	public void saveSaveorder(Salesorder p) {
+	public int saveSaveorder(Salesorder p) {
 	      String sql="INSERT INTO sale_order (sale_date,customer_id,employee_id,status)"
-     + "values('"+p.getSale_date()+"',"+p.getCustomer_id()+","+p.getEmployee_id()+","+p.getStatus()+")";
-	      System.out.println("FECHA -->> " + p.getSale_date()  );
+     + "values('"+p.getSale_date()+"',"+p.getCustomer_id()+","+p.getEmployee_id()+","+p.getStatus()+")";	    
 	System.out.println(sql);
-	template.update(sql);		
-		
+	template.update(sql);	
+	int  ok = template.queryForObject("SELECT MAX(sale_order_id) FROM sale_order", int.class);	
+	System.out.println( "que se inserto " + ok);
+		return ok;	
 	}
 	
 	@Override
-	public void saveSaveorderd(Saleorderdetall s, long productid) {
+	public void saveSaveorderd(Saleorderdetall s, long productid,int result) {
 		 String sql="INSERT INTO sale_order_detail (sale_order_id,product_id,quantity,price,currency,status)"
-			     + "values("+productid+","+s.getProduct_id()+","+s.getQuantity()+","+s.getPrice()+",'"+s.getCurrency()+"',"+s.getStatus()+")";
+			     + "values("+result+","+s.getProduct_id()+","+s.getQuantity()+","+s.getPrice()+",'"+s.getCurrency()+"',"+s.getStatus()+")";
 	System.out.println(sql);
-	template.update(sql);		
+	template.update(sql);	
+	
 	}
+	
+	
 
 	@Override
 	public List<Salesorder> getallSaveorder(int page, int size, String DateFrom, String DateTo) {
 		String sql = "SELECT sale_order_id, sale_date,customer_id, employee_id,status FROM sale_order where (sale_date BETWEEN "
 				+ "'" + DateFrom + "'" + " AND " + "'" + DateTo + "'" + ") LIMIT " + "" + page + ", " + "" + size + "";
+		System.out.println(sql);
 		return template.query(sql, new ResultSetExtractor<List<Salesorder>>() {
 			public List<Salesorder> extractData(ResultSet rs) throws SQLException, DataAccessException {
 				List<Salesorder> list = new ArrayList<Salesorder>();
